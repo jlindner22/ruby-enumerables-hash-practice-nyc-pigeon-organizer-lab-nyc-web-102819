6153fd195a072da7319508e5761ff0755b7b1dcc
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 23:29:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final = {}
+  data.each do |regular, alternate|
+    alternate.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  
+  
+  
 end
+
+#for each pigeon, list their name > color, gender, lives in an array (key)
