4504d63418252947632fa7bb050929ab02812f5f
Jen Lindner <jenlindner22@gmail.com>
2019-10-22 02:50:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,27 @@
 def nyc_pigeon_organizer(data)
   final = {}
+males = data[:gender][:male]
+females = data[:gender][:female]
+names = males + females
+names.each {|name| final[name] = {}
+}
+pigeon_color = data[:color]
+pigeon_color.each {|color_pair|
+  color = color_pair[0]
+data[:color][color].each {|name|
+final[name][:color] = [color]
+}
+pigeon_location = data[:lives]
+pigeon_location.each {|location_pair|
+  location = location_pair[0]
+  data[:lives][location].each {|name|
+  final[name][:lives] = [location]
+p location
+}
+}
+}
 
+puts final
     
 end
 
